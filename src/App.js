f0d3f81646c7cdfc362ecab1d891b6ac5de543e5
oscar L570 <oscardani.dev@outoolcom>
2023-08-14 01:57:03 -0600

    creacion de formulario css y js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/header/Header';
+import Formulario from './componentes/formulario/Formulario';
 
 function App() { 
   return (
     <div>
       {/* 2 maneras de llamar los componentes */}
        <Header></Header>
-      
+       <Formulario/>
     </div>
   );
 }
