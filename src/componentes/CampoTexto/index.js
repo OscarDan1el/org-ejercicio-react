4d7a7410dcffca6915defee1f8168bf0862c92db
oscar L570 <oscardani.dev@outoolcom>
2023-08-15 00:01:32 -0600

    estilizando el formulario

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,9 @@
+import "./CampoTexto.css"
+const CampoTexto = () => {
+    return <div className="campo-texto">
+        <label>Nombre</label>
+        <input placeholder="Ingresar Nombre "></input>
+    </div>
+}
+
+export default CampoTexto;
