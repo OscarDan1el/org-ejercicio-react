6e114fa2a523bfbb3f3c7e6a7641c876fbe78abf
oscar L570 <oscardani.dev@outoolcom>
2023-08-16 16:29:27 -0600

    manejando el evento - Spa - Boton

--- src/componentes/formulario/Formulario.js
+++ src/componentes/formulario/Formulario.js
@@ -1,15 +1,23 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaOpciones from "../ListaOpciones";
+import Boton from "../Boton";
 
 const Formulario = () => {
+
+    const manejarEnvio = (evento) =>{
+        evento.preventDefault()
+        console.log("Manejar el envio", evento);
+    }
+
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador</h2>
             <CampoTexto titulo="Nombre" placeholder="Ingresa Nombre"/>
             <CampoTexto titulo="Puesto" placeholder="Ingresa Puesto"/>
             <CampoTexto titulo="Foto" placeholder="Ingresar enlace de Foto"/>
             <ListaOpciones />
+            <Boton texto="Crear"/>
         </form>
     </section>
 }
