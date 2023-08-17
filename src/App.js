94cca5fa4751984c871f5f615c1da126ca97041b
oscar L570 <oscardani.dev@outoolcom>
2023-08-16 18:32:18 -0600

    elemento Org y su estilo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/header/Header';
 import Formulario from './componentes/formulario/Formulario';
+import MiOrg from './componentes/MiOrg';
 
 function App() { 
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* 2 maneras de llamar los componentes */}
        <Header></Header>
        <Formulario/>
+       <MiOrg/>
     </div>
   );
 }
---
+++ src/componentes/MiOrg/index.js
@@ -0,0 +1,10 @@
+import "./MiOrg.css";
+
+const miOrg = () => {
+    return <section className="orgSection">
+        <h3 className="title">Mi Organizacion</h3>
+        <img src="/img/add.png" alt="add"/>
+    </section>
+}
+
+export default miOrg;
