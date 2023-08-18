4abe4a3a665d91982a101616dbed31fc88da0fb8
oscar L570 <oscardani.dev@outoolcom>
2023-08-17 18:20:44 -0600

    iniciando con state en react

--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,9 +1,28 @@
+import { useState } from "react"; 
 import "./MiOrg.css";
 
 const miOrg = () => {
+     /* Estado -- hooks  */
+    /* useState */
+    /* useState() */
+    console.log()
+
+    /* significa el valor inicial del estado  
+    le damos un nombre.
+    es myu imortante estio debidoa que react reacciona a los cambios del estado*/
+    /* Const [nombreVariable,fucnion que actualize] */
+    
+    const [nombre,actuzalizarNombre] = useState("Harland");
+    const [mostrar,actuzalizarMostrar] = useState(true);
+
+    const manejarClick = () => {
+        console.log("mostrar/ocultar elemento", !mostrar);
+        actuzalizarMostrar(!mostrar); 
+    }
+
     return <section className="orgSection">
-        <h3 className="title">Mi Organizacion</h3>
-        <img src="/img/add.png" alt="add"/>
+        <h3 className="title">Mi Organizacion {nombre}</h3>
+        <img src="/img/add.png" alt="add" onClick={manejarClick}/>
     </section>
 }
 
