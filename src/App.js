624beec31d901b31196db51512b405fc5b9e8466
oscar L570 <oscardani.dev@outoolcom>
2023-08-21 18:08:28 -0600

    2 modulo creando el componente equipo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 import Header from './componentes/header/Header';
 import Formulario from './componentes/formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
 
 function App() { 
-  const [mostrarFormulario,actualizarMostar] = useState(true);
+  /* con true o false puedo determinar e estado de muestra de mi formulario  */
+  const [mostrarFormulario,actualizarMostar] = useState(false);
 
   const cambiarMostrar = () => {
     actualizarMostar(!mostrarFormulario)
@@ -16,8 +18,13 @@ function App() {
     <div>
       {/* 2 maneras de llamar los componentes */}
        <Header></Header>
-       { mostrarFormulario === true ? <Formulario/> : <div></div>}
+       {mostrarFormulario && <Formulario/>}
        <MiOrg cambiarMostrar={cambiarMostrar}/>
+       <Equipo equipo="Programacion"/>
+       <Equipo equipo="Front End"/>
+       <Equipo equipo="Data Science"/>
+       <Equipo equipo="Devops"/>
+       <Equipo equipo="Ux y Diseño"/>
     </div> 
   );
 }
