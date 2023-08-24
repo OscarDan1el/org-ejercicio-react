ae1b0a296d232336e9825d0cd495036891cf6585
oscar L570 <oscardani.dev@outoolcom>
2023-08-23 19:23:17 -0600

    array equipo con map

--- src/App.js
+++ src/App.js
@@ -14,17 +14,61 @@ function App() {
     actualizarMostar(!mostrarFormulario)
   }
 
+  const equipos = [
+    {titulo : "Programacion",
+    colorPrimario : "#57C278",
+    colorSecundario : "D9F7E9"
+    }
+    ,
+    {titulo : "Frontend",
+    colorPrimario : "#82CFFA",
+    colorSecundario : "#E8F8FF"
+    }
+    ,
+    {titulo : "Data Science",
+    colorPrimario : "#A6D157",
+    colorSecundario : "#F0F8E2"
+    }
+    ,
+    {titulo : "Devops",
+    colorPrimario : "#E06B69",
+    colorSecundario : "#FDE7E8"
+    }
+    ,
+    {titulo : "UX y Diseño",
+    colorPrimario : "#DB6EBF",
+    colorSecundario : "#FAE9F5"
+    }
+    ,
+    {titulo : "Movil",
+    colorPrimario : "#FFBA05",
+    colorSecundario : "#FFF5D9"
+    }
+    ,
+    {titulo : "Innovacion y Gestion",
+    colorPrimario : "#FF8A29",
+    colorSecundario : "#FFEEDF"
+    }
+    ,
+    ]
+
   return (
     <div>
       {/* 2 maneras de llamar los componentes */}
        <Header></Header>
        {mostrarFormulario && <Formulario/>}
        <MiOrg cambiarMostrar={cambiarMostrar}/>
-       <Equipo equipo="Programacion"/>
-       <Equipo equipo="Front End"/>
-       <Equipo equipo="Data Science"/>
-       <Equipo equipo="Devops"/>
-       <Equipo equipo="Ux y Diseño"/>
+       
+       {/* 
+       1 nombre de la funcion 
+       2-un metodo y siempre en un arreglo se usa map (recibira datos y nos regresara 
+         un nuevo arreglo y recibira un arrow fuctions)
+       3-
+        */}
+      {
+        equipos.map((equipo) => <Equipo datos={equipo} key={equipo.titulo}/> )
+      } 
+
     </div> 
   );
 }
--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,8 +1,22 @@
 import "./Equipo.css"
 
+
+
 const Equipo = (props) => {
- return <section className="equipo">
-    <h3>{props.equipo}</h3>
+   /* Destructuracion */
+   /* obtengo los valores y creo las variables */
+   /* y esto hace que ya no utilice tanto coddigo de props.--- */
+   const {colorPrimario, colorSecundario, titulo} = props.datos 
+
+
+   const obj = {
+      backgroundColor: colorSecundario
+   }
+
+   const estiloTitulo = {borderColor:colorPrimario};
+
+ return <section className="equipo" style={obj}>
+    <h3 style = {estiloTitulo} >{titulo}</h3>
     <div className="colaboradores">
         
     </div>
