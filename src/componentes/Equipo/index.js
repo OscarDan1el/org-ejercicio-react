baf956590b39f2c1e40ab97acb197c1a29fda17f
Oscar <oscardani.dev@outlook.com>
2023-08-29 18:10:07 -0600

    registrando y mostrando colaborador 2da parte

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -13,16 +13,17 @@ const Equipo = (props) => {
    const obj = {
       backgroundColor: colorSecundario
    }
-   console.log(colaboradores.length > 0)
+   /* console.log(colaboradores.length > 0) */
    
    const estiloTitulo = {borderColor:colorPrimario};
 
  return <section className="equipo" style={obj}>
     <h3 style = {estiloTitulo} >{titulo}</h3>
     <div className="colaboradores">
-        {/* {
-         colaboradores.map((colaborador) => <Colaborador></Colaborador>)
-        } */}
+        {
+         colaboradores.map((colaborador, index) => 
+         <Colaborador datos={colaborador} key={index}/>)
+        }
     </div>
  </section>
 }
